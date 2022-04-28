cc48f12af346406d7e70261fdea0fb808b88ad8d
DuckBoy102 <avieln7@gmail.com>
2022-04-28 16:09:00 +0300

    h

---
+++ test/Box.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace test
+{
+    class Box
+    {
+        public string name;
+        public int fat;
+        public int cal;
+        public bool IsHelthy()
+        {
+            return this.cal < 130 && this.fat < 5;
+        }
+        public int GetCal()
+        {
+            return this.cal;
+        }
+        public string GetName()
+        {
+            return this.name;
+        }
+    }
+}
--- test/Program.cs
+++ test/Program.cs
@@ -10,6 +10,34 @@ namespace test
     {
         static void Main(string[] args)
         {
+            string name;
+            int cal;
+            int fat;
+            int count = 0;
+            int maxCal = 0;
+            string nameOfMax = "";
+            Console.WriteLine("please enter name of product");
+            name = Console.ReadLine();
+            while (name != "stop")
+            {
+                Console.WriteLine("please enter caloriers");
+                cal = int.Parse(Console.ReadLine());
+                Console.WriteLine("please enter fat");
+                fat = int.Parse(Console.ReadLine());
+                Box c = new Box(name, cal, fat);
+                if (c.TsHelthy())
+                    count++;
+                if (c.GetCal() > maxCal)
+                {
+                    maxCal = c.GetCal();
+                    nameOfMax = c.GetName();
+                }
+                Console.WriteLine("please enter name of product");
+                name = Console.ReadLine();
+            }
+            Console.WriteLine("please enter name of product");
+            Console.WriteLine(nameOfMax + "has maximum calories");
+
         }
     }
 }
